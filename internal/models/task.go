fcd10fe8b8f22da6b606ce1b48d102e893bad2c8
VikaPaz <vikaqse@gmail.com>
2024-10-01 19:51:29 +0400

    added status const

--- internal/models/task.go
+++ internal/models/task.go
@@ -2,22 +2,29 @@ package models
 
 import "time"
 
+type TaskStatus string
+
+const (
+	InProgress TaskStatus = "in_progress"
+	Done       TaskStatus = "done"
+)
+
 type Task struct {
-	ID          string `validate:"omitempty,uuidv4"`
+	ID          string `validate:"omitempty,uuid4"`
 	Title       string `validate:"required"`
 	Description string
 	Created     time.Time
 	Updated     time.Time
-	Status      string `validate:"oneof=in_progress done"`
-	OwnerID     string `validate:"uuidv4"`
+	Status      TaskStatus `validate:"oneof=in_progress done"`
+	OwnerID     string     `validate:"uuid4"`
 }
 
 type TaskFilter struct {
-	ID          []string `validate:"omitempty,dive,uuidv4"`
+	ID          []string `validate:"omitempty,dive,uuid4"`
 	Title       string
 	Description string
 	Status      string `validate:"omitempty,oneof=in_progress done"`
-	OwnerID     string `validate:"omitempty,uuidv4"`
+	OwnerID     string `validate:"omitempty,uuid4"`
 	// Created     time.Time // after
 	// Updated     time.Time // after
 	TaskSort TaskSort
@@ -35,9 +42,13 @@ type Range struct {
 }
 
 type TaskUpdate struct {
-	ID          string `validate:"uuidv4"`
+	ID          string `validate:"uuid4"`
 	Title       string
 	Description string
 	Status      string `validate:"required,oneof=in_progress done"`
-	OwnerID     string `validate:"omitempty,uuidv4"`
+	OwnerID     string `validate:"omitempty,uuid4"`
+}
+
+func (t TaskStatus) String() string {
+	return string(t)
 }
